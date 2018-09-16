033d005d7a1098d92d315cb47e59283d9785557e
Tapojit Bhattacharya <tapojit.bhattacharya@gmail.com>
2018-09-16 21:10:49 +0530

    common-namin-server checked in

---
+++ common-naming-server/src/main/java/com/evergreenprogrammers/microservices/commonnamingserver/CommonNamingServerApplication.java
@@ -0,0 +1,12 @@
+package com.evergreenprogrammers.microservices.commonnamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonNamingServerApplication.class, args);
+	}
+}
