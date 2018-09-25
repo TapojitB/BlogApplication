2f0b148d50e958d0ff5bfb24fccfd685cafe0f9c
Tapojit Bhattacharya <tapojit.bhattacharya@gmail.com>
2018-09-25 17:46:37 +0530

    added zuul related annotations

--- common-gateway-server/src/main/java/com/evergreenprogrammers/microservices/commongatewayserver/CommonGatewayServerApplication.java
+++ common-gateway-server/src/main/java/com/evergreenprogrammers/microservices/commongatewayserver/CommonGatewayServerApplication.java
@@ -2,8 +2,12 @@ package com.evergreenprogrammers.microservices.commongatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableDiscoveryClient()
+@EnableZuulProxy
 public class CommonGatewayServerApplication {
 
 	public static void main(String[] args) {
