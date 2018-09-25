e72cb2850179b557571dfc1e0c2a849368ed211f
Tapojit Bhattacharya <tapojit.bhattacharya@gmail.com>
2018-09-25 19:50:48 +0530

    multiple annotations added

--- common-config-server/src/main/java/com/evergreenprogrammers/microservices/commonconfigserver/CommonConfigServerApplication.java
+++ common-config-server/src/main/java/com/evergreenprogrammers/microservices/commonconfigserver/CommonConfigServerApplication.java
@@ -2,10 +2,12 @@ package com.evergreenprogrammers.microservices.commonconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer
 @SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
 public class CommonConfigServerApplication {
 
 	public static void main(String[] args) {
--- shape-service/src/main/java/com/evergreenprogrammers/microservices/shapeservice/ShapeServiceApplication.java
+++ shape-service/src/main/java/com/evergreenprogrammers/microservices/shapeservice/ShapeServiceApplication.java
@@ -2,10 +2,12 @@ package com.evergreenprogrammers.microservices.shapeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages="com.evergreenprogrammers.microservices.shapeservice.proxy")
+@EnableDiscoveryClient
 public class ShapeServiceApplication {
 
 	public static void main(String[] args) {
