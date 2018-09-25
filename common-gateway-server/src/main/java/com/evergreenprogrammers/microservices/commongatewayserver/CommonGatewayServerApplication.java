2baf7ca51de795bcb2df84f0ea52bb9324675827
Tapojit Bhattacharya <tapojit.bhattacharya@gmail.com>
2018-09-25 17:29:23 +0530

    common-gateway-server added

---
+++ common-gateway-server/src/main/java/com/evergreenprogrammers/microservices/commongatewayserver/CommonGatewayServerApplication.java
@@ -0,0 +1,12 @@
+package com.evergreenprogrammers.microservices.commongatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonGatewayServerApplication.class, args);
+	}
+}
